746fd5d15ffe97a7fae32bee0606111616a5344e
GiorgioSeguini <giorgio.seguini@mail.polimi.it>
2021-03-01 19:17:53 +0100

    Initial commit

---
+++ src/main/java/it/polimi/ingsw/Product.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw;
+
+public class Product {
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name = "no name";
+
+
+}
