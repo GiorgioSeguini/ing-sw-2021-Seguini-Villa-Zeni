43327b49b416f0a19fe433838353b77e96682579
williamzeni <william.zeni@mail.polimi.it>
2021-04-09 13:18:43 +0200

    Attraverso i test ho scoperto un errore in getResources. Corretto con successo

--- src/test/java/it/polimi/ingsw/model/WareHouseDepotsTest.java
+++ src/test/java/it/polimi/ingsw/model/WareHouseDepotsTest.java
@@ -1,8 +1,8 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.enumeration.ResourceType;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 public class WareHouseDepotsTest {
 
@@ -10,7 +10,28 @@ public class WareHouseDepotsTest {
     void getResources(){
         WareHouseDepots test= new WareHouseDepots();
         assertEquals(test.getResources(),new NumberOfResources());
-
+        Shelf x= new Shelf(ResourceType.Shields,0);
+        x.setUsed(3);
+        test.addExtraShelf(x);
+        assertEquals(test.getResources(), new NumberOfResources(0,3,0,0));
+        x= new Shelf(ResourceType.Coins,0);
+        x.setUsed(2);
+        test.addExtraShelf(x);
+        assertEquals(test.getResources(), new NumberOfResources(0,3,2,0));
+        x= new Shelf(ResourceType.Servants,0);
+        x.setUsed(4);
+        test.addExtraShelf(x);
+        assertEquals(test.getResources(), new NumberOfResources(4,3,2,0));
+        x= new Shelf(ResourceType.Stones,0);
+        x.setUsed(1);
+        test.addExtraShelf(x);
+        assertEquals(test.getResources(), new NumberOfResources(4,3,2,1));
+        x= new Shelf(ResourceType.Stones,0);
+        x.setUsed(1);
+        test.addExtraShelf(x);
+        assertEquals(test.getResources(), new NumberOfResources(4,3,2,2));
+        test.addExtraShelf(new Shelf(1));
+        assertEquals(test.getResources(), new NumberOfResources(4,3,2,2));
 
     }
 }
