7e9816a6ae8ac372c1680c3d5d0fcec64f98d218
williamzeni <william.zeni@mail.polimi.it>
2021-04-10 11:25:27 +0200

    modifichina

--- src/main/java/it/polimi/ingsw/controller/MoveActiveProduction.java
+++ src/main/java/it/polimi/ingsw/controller/MoveActiveProduction.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.ProductionPower;
+import it.polimi.ingsw.model.enumeration.*;
 
 import javax.swing.text.View;
 
@@ -10,7 +11,7 @@ public class MoveActiveProduction extends MoveType{
     ProductionPower[] toActive;
 
     public MoveActiveProduction(Player player, View view, boolean isLastMove, ProductionPower[] toActive) {
-        super(player, view, isLastMove);
+        super(player, view, MoveChoose.ActivateProduction, isLastMove);
         this.toActive = toActive;
     }
 
