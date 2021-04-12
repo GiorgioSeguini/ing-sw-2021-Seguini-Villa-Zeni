7e3e117da09bbe616e4ca34e7749c1310de804fc
GiorgioSeguini <giorgio.seguini@mail.polimi.it>
2021-04-12 10:18:19 +0200

    riorganizzato i file nelle giuste directory

--- src/main/java/it/polimi/ingsw/controller/Parser.java
+++ src/main/java/it/polimi/ingsw/controller/Parser.java
@@ -10,14 +10,15 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import org.junit.Test;
 
 public class Parser {
 
-    public Level ConvertStringToLevel(String levelapp){
+    public static Level ConvertStringToLevel(String levelapp){
         Level level=null;
         switch (levelapp){
             case "ONE": level=Level.ONE; break;
@@ -28,7 +29,7 @@ public class Parser {
         return level;
     }
 
-    public ColorDevCard ConvertStringToColorDevCard(String colorapp){
+    public static ColorDevCard ConvertStringToColorDevCard(String colorapp){
         ColorDevCard color = null;
         switch (colorapp){
             case "BLUE": color=ColorDevCard.BLUE; break;
@@ -40,7 +41,7 @@ public class Parser {
         return color;
     }
 
-    public NumberOfResources ConvertObjectToNumOfRes(JSONObject object, String objname){
+    public static NumberOfResources ConvertObjectToNumOfRes(JSONObject object, String objname){
         JSONObject NumOfRes=(JSONObject) object.get((objname));
         int servants= Math.toIntExact((Long) NumOfRes.get("Servants"));
         int shields= Math.toIntExact((Long) NumOfRes.get("Shields"));
@@ -49,62 +50,5 @@ public class Parser {
 
         return new NumberOfResources(servants,shields,coins,stones);
     }
-
-    @Test
-    public void DevCardParser() throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
-        JSONArray array= (JSONArray) parser.parse(new FileReader("/home/williamzeni/Projects/IngegneriaSoftware/github/src/main/java/it/polimi/ingsw/controller/DevCard.json"));
-        ArrayList<DevelopmentCard> devcards=new ArrayList<>();
-        for(Object x: array){
-            JSONObject DevCard= (JSONObject) x;
-            Level level=ConvertStringToLevel((String) DevCard.get("Level"));
-            ColorDevCard color= ConvertStringToColorDevCard((String) DevCard.get("CardColor"));
-            NumberOfResources cost= ConvertObjectToNumOfRes(DevCard,"Cost");
-
-            int victorypoints=Math.toIntExact((Long)DevCard.get("VictoryPoints"));
-            JSONObject ProductionPower=(JSONObject) DevCard.get("ProductionPower");
-            int yourchoicein=Math.toIntExact((Long)ProductionPower.get("YourChoiceIn"));
-            int yourchoiceout=Math.toIntExact((Long)ProductionPower.get("YourChoiceOut"));
-            NumberOfResources inres=ConvertObjectToNumOfRes(ProductionPower,"InRes");
-            NumberOfResources outres=ConvertObjectToNumOfRes(ProductionPower,"OutRes");
-            int faithpointsout= Math.toIntExact((Long)ProductionPower.get("FaithPointsOut"));
-
-            ProductionPower productionPower=new ProductionPower(faithpointsout,outres,inres,yourchoicein,yourchoiceout);
-
-            devcards.add(new DevelopmentCard(level,color,cost,productionPower,victorypoints));
-
-        }
-
-        printDevCards(devcards);
-        
-    }
-
-    public void printDevCards(ArrayList<DevelopmentCard> developmentCards){
-        int i=-1;
-        for(DevelopmentCard x: developmentCards){
-            i++;
-            System.out.println("\nCARD "+i);
-            System.out.println("Level: "+x.getLevel());
-            System.out.println("Color: "+x.getColor());
-            System.out.println("VictoryPoints: "+x.getVictoryPoints());
-            System.out.println("Cost");
-            for(ResourceType y: ResourceType.values()){
-                System.out.println("\t"+y+": "+x.getCost().getAmountOf(y));
-            }
-            System.out.println("Production Power");
-            System.out.println("\tInput Resources");
-            for(ResourceType y: ResourceType.values()){
-                System.out.println("\t\t"+y+": "+x.getProductionPower().getInputRes().getAmountOf(y));
-            }
-            System.out.println("\tOutput Resources");
-            for(ResourceType y: ResourceType.values()){
-                System.out.println("\t\t"+y+": "+x.getProductionPower().getOutputRes().getAmountOf(y));
-            }
-            System.out.println("\t\tFaithPoints out: "+x.getProductionPower().getFaithPointsOut());
-            System.out.println("\t\tYourChoiceIn: "+x.getProductionPower().getOfYourChoiceInput());
-            System.out.println("\t\tYourChoiceOut: "+x.getProductionPower().getOfYourChoiceOutput());
-        }
-    }
-
 
 }
--- src/main/java/it/polimi/ingsw/model/DevelopmentCard.java
+++ src/main/java/it/polimi/ingsw/model/DevelopmentCard.java
@@ -4,6 +4,7 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.enumeration.ColorDevCard;
 import it.polimi.ingsw.model.enumeration.Level;
+import it.polimi.ingsw.model.enumeration.ResourceType;
 
 /**
  * 
@@ -52,6 +53,33 @@ public class DevelopmentCard extends Card {
      */
     public ColorDevCard getColor() {
         return cardColor;
+    }
+
+    @Override
+    public String toString(){
+        String res="";
+        res+="Level: "+getLevel() +"\n";
+        res+="Color: "+getColor() +"\n";
+        res+="VictoryPoints: "+getVictoryPoints() +"\n";
+        res+="Cost" +"\n";
+        for(ResourceType y: ResourceType.values()){
+            res+="\t"+y+": "+getCost().getAmountOf(y) +"\n";
+        }
+        res+="Production Power" +"\n";
+        res+="\tInput Resources" +"\n";
+        for(ResourceType y: ResourceType.values()){
+            res+=("\t\t"+y+": "+getProductionPower().getInputRes().getAmountOf(y) +"\n");
+        }
+        res+="\tOutput Resources" +"\n";
+        for(ResourceType y: ResourceType.values()){
+            res+=("\t\t"+y+": "+getProductionPower().getOutputRes().getAmountOf(y) +"\n");
+        }
+        res+="\t\tFaithPoints out: "+getProductionPower().getFaithPointsOut() +"\n";
+        res+="\t\tYourChoiceIn: "+getProductionPower().getOfYourChoiceInput() +"\n";
+        res+="\t\tYourChoiceOut: "+getProductionPower().getOfYourChoiceOutput() +"\n";
+
+
+        return res;
     }
 
 }
