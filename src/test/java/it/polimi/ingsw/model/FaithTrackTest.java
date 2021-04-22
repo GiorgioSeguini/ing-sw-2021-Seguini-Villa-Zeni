a9f47fc9fed0e99150c1b3689ef26110adf0ec19
GiorgioSeguini <giorgio.seguini@mail.polimi.it>
2021-04-22 10:26:39 +0200

    FaithTrackTest

--- src/test/java/it/polimi/ingsw/model/FaithTrackTest.java
+++ src/test/java/it/polimi/ingsw/model/FaithTrackTest.java
@@ -28,6 +28,29 @@ class FaithTrackTest{
             curr.addPoint();
 
         assertEquals(curr.getFaithPoints(), 5);
+        assertEquals(-1, curr.inspectionNeed());
+
+        for(int i=0; i<3; i++)
+            curr.addPoint();
+
+        assertEquals(curr.getFaithPoints(), 8);
+        assertEquals(0, curr.inspectionNeed());
+
+        assertEquals(2, curr.getVictoryPoints());
+        curr.popeInspection(0);
+        assertEquals(4, curr.getVictoryPoints());
+
+        curr.popeInspection(1);
+        assertEquals(4, curr.getVictoryPoints());
+
+        assertFalse(curr.isEnd());
+
+        for(int i=0; i<16; i++)
+            curr.addPoint();
+
+        assertTrue(curr.isEnd());
+
     }
+
 
 }
