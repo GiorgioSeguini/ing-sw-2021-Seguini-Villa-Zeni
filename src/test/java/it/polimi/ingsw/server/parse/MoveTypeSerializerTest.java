25d294f5baf3908050646df1e9c055d0e5c1ef81
williamzeni <william.zeni@mail.polimi.it>
2021-05-06 13:12:39 +0200

    test sistemati

--- src/test/java/it/polimi/ingsw/server/parse/MoveTypeSerializerTest.java
+++ src/test/java/it/polimi/ingsw/server/parse/MoveTypeSerializerTest.java
@@ -1,8 +1,5 @@
 package it.polimi.ingsw.server.parse;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import it.polimi.ingsw.server.controller.MoveActiveProduction;
 import it.polimi.ingsw.server.controller.MoveEndTurn;
 import it.polimi.ingsw.server.controller.MoveType;
@@ -11,7 +8,6 @@ import it.polimi.ingsw.server.model.Player;
 import it.polimi.ingsw.server.model.ProductionPower;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MoveTypeSerializerTest {
@@ -23,7 +19,7 @@ public class MoveTypeSerializerTest {
         productionPowers.add(new ProductionPower(2, new NumberOfResources(0,1,2,3), new NumberOfResources(3,2,4,0)));
         productionPowers.add(new ProductionPower(1, new NumberOfResources(1,0,2,3), new NumberOfResources(3,0,4,0), 3,1));
 
-        MoveType move= new MoveActiveProduction(new Player("Pippo"), productionPowers);
+        MoveType move= new MoveActiveProduction(new Player("lol").getID(), productionPowers);
         String message=Starter.toJson(move);
 
         System.out.println(message);
@@ -32,7 +28,7 @@ public class MoveTypeSerializerTest {
 
     @Test
     public void test2(){
-        MoveType move= new MoveEndTurn(new Player("pippo"));
+        MoveType move= new MoveEndTurn(new Player("ciao").getID());
         System.out.println(Starter.toJson(move));
     } // TODO: 5/6/21 change
 
