fa04eddcd458fe695ad272a0c2a4d5c4b553d53a
fabiovillaa <fabio17.villa@mail.polimi.it>
2021-05-20 16:12:05 +0200

    whitemove

--- src/main/java/it/polimi/ingsw/client/cli/CliMoveWhiteConversion.java
+++ src/main/java/it/polimi/ingsw/client/cli/CliMoveWhiteConversion.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.client.cli;
 
 import it.polimi.ingsw.client.modelClient.GameClient;
+import it.polimi.ingsw.constant.enumeration.ResourceType;
 import it.polimi.ingsw.constant.move.MoveType;
 import it.polimi.ingsw.constant.move.MoveWhiteConversion;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CliMoveWhiteConversion implements CliInterface{
@@ -16,7 +18,75 @@ public class CliMoveWhiteConversion implements CliInterface{
     @Override
     public MoveType updateCLI(GameClient game, Scanner stdin) {
         System.out.println("Come vuoi convertire le biglie bianche?");
-
+        ArrayList<ResourceType> resourceTypes = new ArrayList<>(game.getMe().getConverter().getWhite());
+        int howMuchMarbles=game.getMe().getConverter().getWhite();
+        while(howMuchMarbles!=0) {
+            if (howMuchMarbles == 1) {
+                int typeofres;
+                do {
+                    System.out.println("Hai una sola biglia bianca da convertire.\nIn che cosa vuoi convertirla?");
+                    int i = 1;
+                    for (ResourceType resourceType : game.getMe().getConverter().getToconvert()) {
+                        System.out.println(i + ". " + resourceType);
+                        i++;
+                    }
+                    typeofres = stdin.nextInt();
+                } while (typeofres <= 0 || typeofres > game.getMe().getConverter().getToconvert().size());
+                howMuchMarbles--;
+                resourceTypes.add(game.getMe().getConverter().getToconvert().get(typeofres - 1));
+            } else {
+                System.out.println("Hai ancora " + howMuchMarbles + " biglie bianche da convertire.\nCome vuoi convertirle?");
+                int i = 1;
+                for (ResourceType resourceType : game.getMe().getConverter().getToconvert()) {
+                    System.out.println(i + ". " + resourceType);
+                    i++;
+                }
+                int typeofres = stdin.nextInt();
+                if (typeofres <= 0 || typeofres > i) {
+                    System.out.println("Invalid index!");
+                } else {
+                    System.out.println("Quante biglie vuoi convertire in " + game.getMe().getConverter().getToconvert().get(typeofres - 1) + "?");
+                    int num = stdin.nextInt();
+                    if (num < 0 || howMuchMarbles - num < 0) {
+                        System.out.println("Invalid index!");
+                    } else {
+                        howMuchMarbles -= num;
+                        for (int j = 0; j < num; j++) {
+                            resourceTypes.add(game.getMe().getConverter().getToconvert().get(typeofres - 1));
+                        }
+                    }
+                }
+            }
+            if(howMuchMarbles==0){
+                if(game.getMe().getConverter().getWhite()==1){
+                    System.out.println("Hai scelto di convertire la biglia bianca in "+resourceTypes.get(0)+"\n\tConfermi?\n1. YES\t2. NO");
+                    int confirm=stdin.nextInt();
+                    if(confirm==1){
+                        move.setWhiteMarbles(resourceTypes);
+                    }else{
+                        howMuchMarbles=game.getMe().getConverter().getWhite();
+                        resourceTypes = new ArrayList<ResourceType>();
+                    }
+                }else {
+                    System.out.println("Hai scelto di convertire le " + game.getMe().getConverter().getWhite() + "biglie bianche in questo modo: ");
+                    for(ResourceType r: ResourceType.values()){
+                        int count=0;
+                        for(ResourceType res: resourceTypes){
+                            if(res.equals(r)) count++;
+                        }
+                        System.out.println(count+" "+r);
+                    }
+                    System.out.println("Confermi?\n1. YES\t2. NO");
+                    int confirm=stdin.nextInt();
+                    if(confirm==1){
+                        move.setWhiteMarbles(resourceTypes);
+                    }else{
+                        howMuchMarbles=game.getMe().getConverter().getWhite();
+                        resourceTypes = new ArrayList<ResourceType>();
+                    }
+                }
+            }
+        }
         return move;
     }
 
