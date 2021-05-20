0133921eb9e084a645c86d934d21a0fb97f4bf21
fabiovillaa <fabio17.villa@mail.polimi.it>
2021-05-20 16:51:08 +0200

    finite mosse cli

--- src/main/java/it/polimi/ingsw/client/cli/CliMoveChoseResources.java
+++ src/main/java/it/polimi/ingsw/client/cli/CliMoveChoseResources.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.client.cli;
 
 import it.polimi.ingsw.client.modelClient.GameClient;
+import it.polimi.ingsw.constant.enumeration.ResourceType;
+import it.polimi.ingsw.constant.model.NumberOfResources;
+import it.polimi.ingsw.constant.model.PersonalBoard;
 import it.polimi.ingsw.constant.move.MoveChoseResources;
 import it.polimi.ingsw.constant.move.MoveType;
 
@@ -15,9 +18,30 @@ public class CliMoveChoseResources implements CliInterface{
     }
     @Override
     public MoveType updateCLI(GameClient game, Scanner stdin) {
-        System.out.println("Scegli la risorsa!");
+        NumberOfResources numberOfResources = new NumberOfResources();
+        if(game.getMe().getToActive().getOfYourChoiceInput()==1){
+            System.out.println("Scegli la risorsa in input!");
+            printRes();
+            int res = stdin.nextInt();
+            move.setOfYourChoiceInput(numberOfResources.add(ResourceType.values()[res-1],1));
+        }else{
+            System.out.println("Scegli le risorse in input!");
+            printRes();
+            while(numberOfResources.size()==game.getMe().getToActive().getOfYourChoiceInput()){
+                int res = stdin.nextInt();
+                if(res>0 && res<=ResourceType.values().length){
+                    numberOfResources.add(ResourceType.values()[res-1],1);
 
-
+                }else{
+                    System.out.println("Indice non valido");
+                }
+            }
+            move.setOfYourChoiceInput(numberOfResources);
+        }
+        System.out.println("Scegli la risorsa in output!");
+        printRes();
+        int res = stdin.nextInt();
+        move.setOfYourChoiceOutput(numberOfResources.add(ResourceType.values()[res-1],1));
         return move;
     }
 
@@ -29,5 +53,13 @@ public class CliMoveChoseResources implements CliInterface{
     @Override
     public String getName() {
         return move.getClassName();
+    }
+
+    private void printRes(){
+        int i=1;
+        for(ResourceType type : ResourceType.values()){
+            System.out.println(i+". "+type);
+            i++;
+        }
     }
 }
