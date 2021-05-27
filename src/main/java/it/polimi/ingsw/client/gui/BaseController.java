62f8bcdc6654c6d447d0f7f94b4e47342f9af624
GiorgioSeguini <giorgio.seguini@mail.polimi.it>
2021-05-27 14:30:21 +0200

    aggiunti fxml da fare

---
+++ src/main/java/it/polimi/ingsw/client/gui/BaseController.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.client.gui;
+
+public class BaseController extends ControllerGuiInterface{
+
+    public static String className = "base";
+
+    @Override
+    public String getName() {
+        return className;
+    }
+
+    @Override
+    public void update() {
+        //TODO
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/client/gui/InitialResController.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.client.gui;
+
+public class InitialResController extends ControllerGuiInterface{
+
+    public static String className = "initialRes";
+
+    @Override
+    public String getName() {
+        return className;
+    }
+
+    @Override
+    public void update() {
+        //TODO
+    }
+}
