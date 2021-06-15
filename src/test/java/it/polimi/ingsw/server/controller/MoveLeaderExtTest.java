8a7e52dab8877028f301cb35207d91cf4e5dfc2c
fabiovillaa <fabio17.villa@mail.polimi.it>
2021-06-15 19:16:37 +0200

    MoveAutoPlayTest

--- src/test/java/it/polimi/ingsw/server/controller/MoveLeaderExtTest.java
+++ src/test/java/it/polimi/ingsw/server/controller/MoveLeaderExtTest.java
@@ -1,16 +1,11 @@
 package it.polimi.ingsw.server.controller;
 import it.polimi.ingsw.constant.model.NumberOfResources;
-import it.polimi.ingsw.constant.model.Player;
-import it.polimi.ingsw.constant.model.ProductionPower;
 import it.polimi.ingsw.server.model.*;
 import it.polimi.ingsw.constant.enumeration.*;
-import it.polimi.ingsw.server.model.exception.NoSpaceException;
-import it.polimi.ingsw.server.model.exception.OutOfResourcesException;
 import it.polimi.ingsw.server.parse.Starter;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
