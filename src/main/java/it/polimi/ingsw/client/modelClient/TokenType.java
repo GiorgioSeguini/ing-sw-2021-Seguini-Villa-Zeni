dec2b3d2787421c1818519a8542066ec24ce4002
williamzeni <william.zeni@mail.polimi.it>
2021-06-16 19:14:28 +0200

    gestita print del soloplayer nella CLI

--- src/main/java/it/polimi/ingsw/client/modelClient/TokenType.java
+++ src/main/java/it/polimi/ingsw/client/modelClient/TokenType.java
@@ -1,10 +1,21 @@
 package it.polimi.ingsw.client.modelClient;
 
 public enum TokenType {
-    DISCARD2_BLUE,
-    DISCARD2_GREEN,
-    DISCARD2_PURPLE,
-    DISCARD2_YELLOW,
-    MOVE2,
-    MOVE_SHUFFLE
+    DISCARD2_BLUE("Lorenzo ha scartato 2 carte sviluppo blu."),
+    DISCARD2_GREEN("Lorenzo ha scartato 2 carte sviluppo verdi."),
+    DISCARD2_PURPLE("Lorenzo ha scartato 2 carte sviluppo viola."),
+    DISCARD2_YELLOW("Lorenzo ha scartato 2 carte sviluppo gialle."),
+    MOVE2("Lorenzo è avanzato di due caselle sul tracciato fede. "),
+    MOVE_SHUFFLE("Lorenzo è avanzato di una casella sul tracciato fede e ha rimescolato i Segnalini Azione.");
+
+
+    private String textToPrint;
+
+    TokenType(String textToPrint){
+        this.textToPrint= textToPrint;
+    }
+
+    public String getTextToPrint() {
+        return textToPrint;
+    }
 }
