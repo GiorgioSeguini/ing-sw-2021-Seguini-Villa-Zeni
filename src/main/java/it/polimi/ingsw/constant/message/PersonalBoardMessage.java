bd26cf7249b3438ce3001702793140f05b379a7c
GiorgioSeguini <giorgio.seguini@mail.polimi.it>
2021-06-16 16:39:03 +0200

    cambiato PersonalBoardMessage, ora gli altri non conoscono più le tue leader card

--- src/main/java/it/polimi/ingsw/constant/message/PersonalBoardMessage.java
+++ src/main/java/it/polimi/ingsw/constant/message/PersonalBoardMessage.java
@@ -4,6 +4,7 @@ import com.google.gson.reflect.TypeToken;
 import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.modelClient.*;
 import it.polimi.ingsw.client.parser.StarterClient;
+import it.polimi.ingsw.constant.enumeration.LeaderStatus;
 import it.polimi.ingsw.constant.model.*;
 
 import java.lang.reflect.Type;
@@ -24,6 +25,16 @@ public class PersonalBoardMessage implements Message{
     public void handleMessage(Client client){
         Game simpleGame = client.getSimpleGame();
         Player owner = simpleGame.getPlayerFromID(IDplayer);
+
+        if(client.getSimpleGame().getMyID()!=IDplayer){
+            ArrayList<LeaderCard> cards = new ArrayList<>();
+            for(LeaderCard card : personalBoard.getLeaderCards()){
+                if(card.getStatus()== LeaderStatus.Played)
+                    cards.add(card);
+            }
+            personalBoard.setLeaderCards(cards);
+        }
+
         owner.setPersonalBoard(personalBoard);
     }
 
