4d70057598d8e4521c29e343664853f7a6b8e090
GiorgioSeguini <giorgio.seguini@mail.polimi.it>
2021-06-27 21:47:37 +0200

    jar

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -8,9 +8,15 @@ public class ClientApp
 {
     public static void main(String[] args){
         int ui = 1;
+        int port = 12345;           //default values
+        String ip = "127.0.0.1";
+        if(args.length>1)
+            ip = args[0];
+        if(args.length>2)
+            port = Integer.parseInt(args[1]);
         if(args.length<3 || args[2].equals("gui"))
             ui = 2;
-        Client client = new Client(args[0], Integer.parseInt(args[1]), ui);
+        Client client = new Client(ip, port, ui);
         try{
             client.run();
         }catch (IOException e){
